8be2a91d26af504d5da250b0c536420083d4edb8
raygervais <raygervais@hotmail.ca>
2022-04-16 12:09:24 -0400

    WIP template driven API

--- cmd/dfw/main.go
+++ cmd/dfw/main.go
@@ -23,3 +23,37 @@ func main() {
 	email.Thread(&db)
 
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"html/template"
+// 	"log"
+// 	"net/http"
+// 	"path/filepath"
+// 	"strings"
+// )
+
+// func main() {
+// 	fs := http.FileServer(http.Dir("./static"))
+// 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+// 	http.HandleFunc("/", serveTemplate)
+
+// 	log.Println("Listening on :3000...")
+// 	err := http.ListenAndServe(":3000", nil)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
+
+// func serveTemplate(w http.ResponseWriter, r *http.Request) {
+// 	lp := filepath.Join("pkg", "templates", "base.html")
+// 	fp := filepath.Join("pkg", "templates", filepath.Clean(strings.Split(r.URL.Path, ".")[0]+".html"))
+
+// 	fmt.Println(fp)
+
+// 	tmpl, _ := template.ParseFiles(lp, fp)
+// 	tmpl.ExecuteTemplate(w, "layout", nil)
+// }
--- pkg/smtp/smtp.go
+++ pkg/smtp/smtp.go
@@ -1,57 +1,28 @@
 package smtp
 
-import (
-	"fmt"
-	"net/smtp"
-	"time"
-
-	"github.com/raygervais/dfw/pkg/db"
-)
+import "net/smtp"
 
 type SMTP struct {
-	Host string
-	Port int
-	Auth smtp.Auth
 }
 
-func Thread(db *db.Database) {
-	client := SMTP{
-		Host: "smtp.gmail.com",
-		Port: 587,
-		Auth: smtp.PlainAuth("", "", "", ""),
+func (s *SMTP) Send(to string, subject string, body string) error {
+
+	return nil
+}
+
+// Send email using smtp package
+func Send(to string, subject string, body string) error {
+	// Set up authentication information.
+	auth := smtp.PlainAuth("", "user", "password", "smtp.example.com")
+
+	// Connect to the server, authenticate, set the sender and recipient,
+	// and send the email all in one step.
+	err := smtp.SendMail("smtp.example.com:25", auth, "", []string{to}, []byte(body))
+
+	// Check for errors
+	if err != nil {
+		return err
 	}
 
-	for {
-		expiredRooms := db.ListExpiredRooms()
-		for _, room := range expiredRooms {
-
-			// Create a message which contains the contents of all comments in the room
-			message := ""
-			for _, comment := range room.Comments {
-				message += comment.Comment + "\n"
-			}
-
-			// Send the message
-			if err := client.Send("Room Comments for Prompt: "+room.Prompt.Text, []string{room.HostEmail},
-				client.Message("Room Expired", room.HostEmail, "", message)); err != nil {
-				fmt.Print(err)
-			}
-
-			db.RemoveRoom(room.Id)
-		}
-
-		time.Sleep(time.Minute)
-
-	}
-}
-
-func (s *SMTP) Message(subject, to, from, msg string) []byte {
-	return []byte("From: " + from + "\n" +
-		"To: " + to + "\n" +
-		"Subject: " + subject + "\n\n" +
-		msg)
-}
-
-func (s *SMTP) Send(from string, to []string, msg []byte) error {
-	return smtp.SendMail(s.Host+":"+string(s.Port), s.Auth, from, to, msg)
+	return nil
 }
